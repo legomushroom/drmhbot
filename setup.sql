c23352b2b11ae84a1cb06afdeb13a3a949cb2efe
Julia <juliariety@gmail.com>
2021-02-22 12:03:24 +0100

    Set up the database in CI

---
+++ setup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sources (
+    name     VARCHAR(255) NOT NULL UNIQUE,
+    hostname VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE posts (
+    title     VARCHAR(255) NOT NULL,
+    url       VARCHAR(255) NOT NULL,
+    source    VARCHAR(255) NOT NULL,
+    important BOOLEAN      NOT NULL,
+    italic    BOOLEAN      NOT NULL,
+);
