c54a1c12660ade6682139c3bf6bbaf9709e20790
Julia <juliariety@gmail.com>
2021-08-18 12:59:07 +0000

    Remove unneeded datbase from CI
    Closes #42

--- setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE posts (
-    title     VARCHAR(255) NOT NULL,
-    url       VARCHAR(255) NOT NULL,
-    source    VARCHAR(255) NOT NULL,
-    important BOOLEAN      NOT NULL,
-    italic    BOOLEAN      NOT NULL
-);
